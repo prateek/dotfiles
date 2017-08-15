fd39c91904f5969b501c61cb91b3cbbaa3f99a84
Prateek Rungta <prateek86400@gmail.com>
2017-08-15 13:49:17 -0400

    better folder management

--- zsh-plugins/install.sh
+++ zsh-plugins/install.sh
@@ -5,6 +5,11 @@
 # Note: This uses ZSH specific shell features, but considering this is for zsh plugin install, w/e.
 # Author: Prateek Rungta
 
+CWD=$(dirname "$0")
+RAW_FOLDER=$CWD/raw
+FUNCTION_FOLDER=$CWD/zfunctions
+COMPLETION_FOLDER=$CWD/completions
+
 typeset -A targets
 targets=(
   "async"              "https://raw.githubusercontent.com/sindresorhus/pure/master/async.zsh"       \
@@ -12,15 +17,29 @@ targets=(
   "hub_completions"    "https://raw.githubusercontent.com/github/hub/master/etc/hub.zsh_completion" \
 )
 
-install_date=$(date +%Y%m%d%H%M)
+typeset -A folders
+folders=(
+  "async"              "$FUNCTION_FOLDER"   \
+  "prompt_pure_setup"  "$FUNCTION_FOLDER"   \
+  "hub_completions"    "$COMPLETION_FOLDER" \
+)
 
+mkdir -p $RAW_FOLDER $FUNCTION_FOLDER $COMPLETION_FOLDER
+install_date=$(date +%Y%m%d%H%M)
 for name in "${(@k)targets}"; do
   url=$targets[$name]
+  folder=$folders[$name]
   echo "Installing $name: from $url"
-  rm -f ${name}-*
-  local_file=${name}-${install_date}
-  curl -o $local_file $url
-  chmod +x $local_file
-  rm -f ./zfunctions/$name
-  ln -s $PWD/$local_file ./zfunctions/$name
+
+  # clear existing raw file
+  rm -f $RAW_FOLDER/${name}-*
+
+  # download raw file and mark executable
+  raw_file=$RAW_FOLDER/${name}-${install_date}
+  curl -o $raw_file $url
+  chmod +x $raw_file
+
+  # soft-link to target folder
+  rm -f $folder/$name
+  ln -s $raw_file $folder/$name
 done
