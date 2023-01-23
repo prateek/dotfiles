7991236e71e533f46e0a06253b5a7e617475e36d
Prateek Rungta <rungta@uber.com>
2023-01-22 21:00:54 -0500

    wire up sshrc+lesskey

--- boostrap.sh
+++ boostrap.sh
@@ -10,10 +10,22 @@ brew tap Homebrew/bundle
 # setup symlinks
 ln -s $CWD/vimrc $HOME/.vimrc
 ln -s $CWD/zshrc $HOME/.zshrc
+ln -s $CWD/sshrc $HOME/.sshrc
 
 # directories
 mkdir -p $HOME/code
+mkdir -p $HOME/.sshrc.d
 
 # vim setup
 $CWD/install-vim-plug.sh
 vim -c "PlugInstall" #TODO: quit
+
+# generate lesskey binary file for older versions of less that might be
+# present on remote machines.
+# nb: will need to `brew install less` for the following line to work.
+lesskey -o $HOME/.less $CWD/lesskey
+cp $HOME/.less $HOME/.sshrc.d/.less
+
+# TODO:
+# - need to copy and sync $HOME/.config directory too
+# - need to run `autoload zkbd && zkbd` to setup keycode file (used in zshrc)
