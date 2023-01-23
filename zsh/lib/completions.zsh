5d6e7ce845af4f5a2a62cb3271c9174120b934c4
Prateek Rungta <rungta@uber.com>
2023-01-23 01:28:17 -0500

    More cleanup fixmes

--- zsh/lib/completions.zsh
+++ zsh/lib/completions.zsh
@@ -49,24 +49,24 @@ alias egrep='egrep --color=auto'
 alias sz='source ~/.zshrc'
 alias ez='vim ~/.zshrc'
 
-# Pipe Aliases
-alias L=' | less '
-alias G=' | egrep --color=auto '
-alias T=' | tail '
-alias H=' | head '
-alias W=' | wc -l '
-alias S=' | sort '
-
-globalias() {
-   if [[ $LBUFFER =~ ' [A-Z0-9]+$' ]]; then
-     zle _expand_alias
-     zle expand-word
-   fi
-   zle self-insert
-}
-
-zle -N globalias
-
-bindkey " " globalias
-bindkey "^ " magic-space           # control-space to bypass completion
-bindkey -M isearch " " magic-space # normal space during searches
+# FIXME: Pipe Aliases
+# alias L=' | less '
+# alias G=' | egrep --color=auto '
+# alias T=' | tail '
+# alias H=' | head '
+# alias W=' | wc -l '
+# alias S=' | sort '
+#
+# globalias() {
+#    if [[ $LBUFFER =~ ' [A-Z0-9]+$' ]]; then
+#      zle _expand_alias
+#      zle expand-word
+#    fi
+#    zle self-insert
+# }
+#
+# zle -N globalias
+#
+# bindkey " " globalias
+# bindkey "^ " magic-space           # control-space to bypass completion
+# bindkey -M isearch " " magic-space # normal space during searches
