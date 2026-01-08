3a07ef8e6bdc39d7e91815019762abc90fb7758b
Prateek Rungta <prateek@openai.com>
2026-01-08 15:42:40 -0500

    zsh: update git log alias to use explicit command

--- zsh/lib/alias.zsh
+++ zsh/lib/alias.zsh
@@ -14,7 +14,8 @@ alias lg="lazygit"
 # via https://stackoverflow.com/questions/1057564/pretty-git-branch-graphs
 alias gl1="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)' --all"
 alias gl2="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(auto)%d%C(reset)%n          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)'"
-alias gl="gl1"
+alias gl="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)'"
+alias gla="gl1"
 
 alias gp='git pull origin $(git rev-parse --abbrev-ref HEAD)'
 alias gpo='git pull origin $(git rev-parse --abbrev-ref HEAD)'
